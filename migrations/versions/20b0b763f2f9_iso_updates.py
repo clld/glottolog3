dde43f917bc12d269c3320174527a934bdfb4e70
xrotwang <xrotwang@googlemail.com>
2014-10-09 13:04:21 +0200

    add geocoords

--- migrations/versions/20b0b763f2f9_iso_updates.py
+++ migrations/versions/20b0b763f2f9_iso_updates.py
@@ -18,6 +18,15 @@ import sqlalchemy as sa
 
 
 def upgrade():
+    """
+gmo     6.57645 37.1381
+stc     -10.6771        165.835
+lto     0.2975  34.6961
+lks     0.1529  34.5708
+lsm     0.3652  34.0335
+lrt     -8.4746 122.7619
+aqz     -12.8322        -60.9716
+    """
     pass
 
 def downgrade():
