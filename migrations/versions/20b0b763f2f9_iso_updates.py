8925c6c3494be7fe1380043aca001ceb70ef6de1
xrotwang <xrotwang@googlemail.com>
2014-10-09 12:51:55 +0200

    iso update

---
+++ migrations/versions/20b0b763f2f9_iso_updates.py
@@ -0,0 +1,25 @@
+# coding=utf-8
+"""iso updates
+
+Revision ID: 20b0b763f2f9
+Revises: 53f4e74ce460
+Create Date: 2014-10-09 12:51:21.612004
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '20b0b763f2f9'
+down_revision = '53f4e74ce460'
+
+import datetime
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+def downgrade():
+    pass
+
