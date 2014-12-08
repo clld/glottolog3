5e7c0569b5dc97375068fb0468bec106873ff990
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2014-12-08 14:11:16 +0100

    ref empty string to null migration

--- migrations/versions/185b48316200_ref_empty_null.py
+++ migrations/versions/185b48316200_ref_empty_null.py
@@ -1,15 +1,15 @@
 # coding=utf-8
 """ref empty null
 
-Revision ID:
-Revises:
-Create Date:
+Revision ID: 185b48316200
+Revises: 4b89d69d005c
+Create Date: 2014-12-08 12:56:45.261000
 
 """
 
 # revision identifiers, used by Alembic.
-revision = ''
-down_revision = ''
+revision = '185b48316200'
+down_revision = '4b89d69d005c'
 
 import datetime
 
@@ -17,14 +17,15 @@ from alembic import op
 import sqlalchemy as sa
 
 
-def upgrade():
+def upgrade(verbose=True):
     conn = op.get_bind()
 
     select_cols = sa.text('SELECT a.attname FROM pg_attribute AS a '
         'JOIN pg_class AS c ON a.attrelid = c.oid '
         'JOIN pg_type AS t ON a.atttypid = t.oid '
         'WHERE c.relname = :table AND t.typname = :type '
-        'AND a.attnum > 0 AND NOT a.attnotnull', conn)
+        'AND a.attnum > 0 AND NOT a.attnotnull '
+        'ORDER BY a.attnum', conn)
 
     def exists_empty(table, col):
         query = "SELECT EXISTS (SELECT 1 FROM %s WHERE %s = '')" % (table, col)
@@ -38,7 +39,8 @@ def upgrade():
         for col, in select_cols.execute(table=table, type='varchar'):
             if exists_empty(table, col).scalar():
                 changed = nullify_empty(table, col).execute().rowcount
-                print table, col, changed
+                if verbose:
+                    print '%s.%s\t%d' % (table, col, changed)
 
 
 def downgrade():
