af72c1286227a48aef23b128861ee72a975794f8
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2015-04-16 17:39:39 +0200

    work on migration to deactivate empty families

---
+++ migrations/pending/deactivate_chin1491_gher1239_yuca1255.py
@@ -0,0 +1,91 @@
+# coding=utf-8
+"""deactivate chin1491 gher1239 yuca1255
+
+Revision ID: 
+Revises: 
+Create Date: 
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = ''
+down_revision = ''
+
+import datetime
+
+from alembic import op
+import sqlalchemy as sa
+
+DEACTIVATE = ['chin1491', 'gher1239', 'yuca1255']
+
+
+def upgrade():
+    conn = op.get_bind()
+
+    lang_deactivate = sa.text('UPDATE language SET updated = now(), '
+        'active = FALSE WHERE id = :id AND active ', conn)
+    uoid_deactivate = sa.text('UPDATE languoid AS ll SET father_pk = NULL '
+        'WHERE father_pk IS NOT NULL AND EXISTS (SELECT 1 FROM language '
+        'WHERE pk = ll.pk AND id = :id )', conn)
+
+    for id in DEACTIVATE:
+        lang_deactivate.execute(id=id)
+        uoid_deactivate.execute(id=id)
+
+    raise NotImplementedError
+
+    for sql in RECREATE_TREECLOSURE:
+        op.execute(sql)
+
+
+def downgrade():
+    pass
+
+
+RECREATE_TREECLOSURE = [
+    """DELETE FROM treeclosuretable""",
+    """WITH RECURSIVE tree(child_pk, parent_pk, depth) AS (
+      SELECT pk, pk, 0 FROM languoid
+    UNION ALL
+      SELECT t.child_pk, p.father_pk, t.depth + 1
+      FROM languoid AS p
+      JOIN tree AS t ON p.pk = t.parent_pk
+      WHERE p.father_pk IS NOT NULL
+    )
+    INSERT INTO treeclosuretable (created, updated, active, child_pk, parent_pk, depth)
+    SELECT now(), now(), true, * FROM tree""",
+    """UPDATE languoid AS l SET family_pk = u.family_pk
+    FROM (WITH RECURSIVE tree(child_pk, parent_pk, depth) AS (
+      SELECT pk, father_pk, 1 FROM languoid
+    UNION ALL
+      SELECT t.child_pk, p.father_pk, t.depth + 1
+      FROM languoid AS p
+      JOIN tree AS t ON p.pk = t.parent_pk
+      WHERE p.father_pk IS NOT NULL
+    )
+    SELECT DISTINCT ON (child_pk) child_pk, parent_pk AS family_pk
+    FROM tree ORDER BY child_pk, depth DESC) AS u
+    WHERE l.pk = u.child_pk AND l.family_pk IS DISTINCT FROM u.family_pk""",
+    """UPDATE languoid AS l SET
+      child_family_count = u.child_family_count,
+      child_language_count = u.child_language_count,
+      child_dialect_count = u.child_dialect_count
+    FROM (WITH RECURSIVE tree(child_pk, parent_pk, level) AS (
+      SELECT pk, father_pk, level FROM languoid
+    UNION ALL
+      SELECT t.child_pk, p.father_pk, t.level
+      FROM languoid AS p
+      JOIN tree AS t ON p.pk = t.parent_pk
+      WHERE p.father_pk IS NOT NULL
+    )
+    SELECT pk,
+      count(nullif(tree.level != 'family', true)) AS child_family_count,
+      count(nullif(tree.level != 'language', true)) AS child_language_count,
+      count(nullif(tree.level != 'dialect', true)) AS child_dialect_count
+    FROM languoid LEFT JOIN tree ON pk = tree.parent_pk
+    GROUP BY pk) AS u
+    WHERE l.pk = u.pk AND (
+      l.child_family_count != u.child_family_count OR
+      l.child_language_count != u.child_language_count OR
+      l.child_dialect_count != u.child_dialect_count)""",
+]
