3ebf502ea9e3f66d6f9836a7bb6213189d71d2be
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2015-04-24 12:15:38 +0200

    fixes clld/glottolog-data#9

--- migrations/versions/2f35007cbe17_same_name_languages.py
+++ migrations/versions/2f35007cbe17_same_name_languages.py
@@ -1,23 +1,25 @@
 # coding=utf-8
 """same name languages
 
-Revision ID: 
-Revises: 
-Create Date: 
+Revision ID: 2f35007cbe17
+Revises: 5b112e7fb9
+Create Date: 2015-04-24 11:30:15.361000
 
 """
 
 # revision identifiers, used by Alembic.
-revision = ''
-down_revision = ''
+revision = '2f35007cbe17'
+down_revision = '5b112e7fb9'
 
 import datetime
 
 from alembic import op
 import sqlalchemy as sa
 
+
 ID_BEFORE_AFTER = [
     ('dama1262', 'Dama', 'Dama (Sierra Leone)'),
+    ('dama1267', 'Dama', 'Dama (Cameroon)'),
     ('gana1268', 'Ganai', 'Birrdhawal'),
     ('mang1429', 'Mango', 'Mango (China)'),
     ('ngal1301', 'Ngala', 'Ngala of Lake Chad'),
@@ -31,12 +33,20 @@ def upgrade():
     update_name = sa.text('UPDATE language SET updated = now(), '
         'name = :after WHERE id = :id AND name = :before')
 
+    params = {'type': 'name', 'description': 'Glottolog', 'lang': 'en'}
+
+    unlink_ident = sa.text('DELETE FROM languageidentifier AS li '
+        'WHERE language_pk = (SELECT pk FROM language WHERE id = :id) '
+        'AND EXISTS (SELECT 1 FROM identifier WHERE pk = li.identifier_pk '
+            'AND type = :type AND description = :description AND lang = :lang '
+            'AND name = :before)').bindparams(**params)
+
     insert_ident = sa.text('INSERT INTO identifier '
         '(created, updated, active, version, type, description, lang, name) '
         'SELECT now(), now(), true, 1, :type, :description, :lang, :name '
         'WHERE NOT EXISTS (SELECT 1 FROM identifier WHERE type = :type '
         'AND description = :description AND lang = :lang AND name = :name)'
-        ).bindparams(type='name', description='Glottolog', lang='en')
+        ).bindparams(**params)
 
     insert_lang_ident = sa.text('INSERT INTO languageidentifier '
         '(created, updated, active, version, language_pk, identifier_pk) '
@@ -48,14 +58,19 @@ def upgrade():
         'WHERE language_pk = (SELECT pk FROM language WHERE id = :id) '
         'AND identifier_pk = (SELECT pk FROM identifier WHERE type = :type '
         'AND description = :description AND lang = :lang AND name = :name))'
-        ).bindparams(type='name', description='Glottolog', lang='en')
+        ).bindparams(**params)
+
+    delete_orphans = sa.text('DELETE FROM identifier AS i '
+        'WHERE NOT EXISTS (SELECT 1 FROM languageidentifier '
+        'WHERE identifier_pk = i.pk)')
 
     for id, before, after in ID_BEFORE_AFTER:
         op.execute(update_name.bindparams(id=id, before=before, after=after))
+        op.execute(unlink_ident.bindparams(id=id, before=before))
         op.execute(insert_ident.bindparams(name=after))
         op.execute(insert_lang_ident.bindparams(id=id, name=after))
 
-    raise NotImplementedError
+    op.execute(delete_orphans)
 
 
 def downgrade():
