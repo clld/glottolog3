8bb8843c53a11d9f96fd946a1bf131323000b429
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2015-05-06 09:40:42 +0200

    fixes clld/glottolog-data#66 fixes clld/glottolog-data#67

--- migrations/versions/4c43b1dc96e1_adapt_russ1263_crim1257_coordinates.py
+++ migrations/versions/4c43b1dc96e1_adapt_russ1263_crim1257_coordinates.py
@@ -1,15 +1,15 @@
 # coding=utf-8
-"""adapt corrdinates
+"""adapt russ1263 crim1257 coordinates
 
-Revision ID: 
-Revises: 
-Create Date: 
+Revision ID: 4c43b1dc96e1
+Revises: 251f19b22119
+Create Date: 2015-05-06 09:31:04.248000
 
 """
 
 # revision identifiers, used by Alembic.
-revision = ''
-down_revision = ''
+revision = '4c43b1dc96e1'
+down_revision = '251f19b22119'
 
 import datetime
 
@@ -17,15 +17,15 @@ from alembic import op
 import sqlalchemy as sa
 
 ID_BEFORE_AFTER = [
-    ('russ1263', ('53.9195', '72.3926')),
-    ('crim1257', ('43.8398', '28.1418')),
+    ('russ1263', ('53.9195', '72.3926'), ('59', '50')),
+    ('crim1257', ('43.8398', '28.1418'), ('45', '34.08')),
 ]
 
 
 def upgrade():
     update_coord = sa.text('UPDATE language SET updated = now(), '
         'latitude = :lat_after, longitude = :lon_after '
-        'WHERE id = :id AND latitude = :lat_before AND longitude = :long_before')
+        'WHERE id = :id AND latitude = :lat_before AND longitude = :lon_before')
 
     for id, (lat_before, lon_before), (lat_after, lon_after) in ID_BEFORE_AFTER:
         op.execute(update_coord.bindparams(id=id,
