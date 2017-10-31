7b4869b5079a216093531600b9cb4917cac47ba5
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2017-10-31 12:44:07 +0100

    fix

--- glottolog3/assets.py
+++ glottolog3/assets.py
@@ -6,5 +6,5 @@ import glottolog3
 _dir = Path(glottolog3.__file__).parent / 'static'
 _url = '/glottolog3:static/'
 
-environment.append_path(_dir, url=_url)
+environment.append_path(str(_dir), url=_url)
 environment.load_path = list(reversed(environment.load_path))
