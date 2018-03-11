155c86f13bd1d5d590c9d7a253dbb175f5e4ed2d
Jeffrey Kam <jeffreykam0415@gmail.com>
2018-03-11 17:28:31 -0400

    retesting

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ setup(
         'markdown',
         'newick>=0.4',
         'pyglottolog~=1.1',
+        'psycopg2~=2.7.4'
     ],
     extras_require={
         'dev': ['flake8', 'wheel', 'twine'],
